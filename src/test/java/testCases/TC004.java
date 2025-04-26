d150b9bca2137df84420fe5da278bfb4947f6ef5
ARUNKUMAR <arungh51@gmail.com>
2025-04-26 15:10:25 +0530

    Initial commit

--- src/test/java/testCases/TC004.java
+++ src/test/java/testCases/TC004.java
@@ -1,6 +1,9 @@
 package testCases;
 
 public class TC004 {
-	//wILL Implement later
+	//wILL Implement later  -0e33683117f54149bf5097cff62286cc
+	//JenkinUrl-  http://localhost:8080/
+	//java -jar jenkins.war
+	//java -jar jenkins.war --enable-future-java
 
 }
